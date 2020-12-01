eb91d66b9dd236844f26ab5321d2499977615996
nedoletoff <nedoletoff@yandex.ru>
2020-12-01 18:42:22 +0300

    Working version

--- f.c
+++ f.c
@@ -5,11 +5,14 @@
 int main()
 {
 	char str[N] = {'\0'};
+	char word[N] = {'\0'};
 	int count = 0;
 	int first = 0;
 	int numw = 0;
 	char** words = NULL;
 	int len = 0;
+	int* arr = NULL;
+	int maxlen = 0;
 
 	printf("Type string: ");
 	for (int i = 0; i < N - 1; ++i)		//считать строку и посчитать пробелы
@@ -17,14 +20,25 @@ int main()
 		str[i] = getchar();
 		if (str[i] == ' ')
 		       ++numw;
-		if (str[i] == '\0' || str[i] == '\n')
+		if (str[i] == '\n')
 		{
 			str[i] = ' ';
 			++numw;
 			break;
 		}
 	}
-	
+
+	printf("Type word: ");
+	for (int i = 0; i < N - 1; ++i)		//считать слово 
+	{
+		word[i] = getchar();
+		if (word[i] == '\n')
+		{
+			word[i] = '\0';
+			break;
+		}
+	}
+
 	words = (char**) malloc(numw * sizeof(char*));		//создать массив адресов
 
 	for (int j = 0; j < N; ++j)		//посчитать количество слов и создать слова
@@ -45,23 +59,35 @@ int main()
 				++first;
 			}
 			words[count][len++] = '\0';
+			if (maxlen < len)
+				maxlen = len - 1;
 			++count;
 		}
 	}
-
+	
+	arr = (int*) calloc(count, sizeof(int));	//создать массив длины слов
+	
 	for (int i = count; i < numw; ++i)	//очистить лишние адреса
 		free(words[i]);
 		
-	for (int i = 0; i < count; ++i)		//вывести слова в нужном порядке
+	for (int i = 0; i < count; ++i)		//посчитать длину слов
 	{
 		len = 0;
 		while (words[i][++len] != '\0');
-		while (len >= 0)
-			putchar(words[i][len--]);
-		printf("\n");
-		free(words[i]);		//очистить слова
+		arr[i] = len;
 	}
+	
+	for (int i = 0; i < count; ++i)		//вывести слова
+	{
+		if (arr[i] != maxlen)
+			puts(words[i]);
+		else 
+			puts(word);
+		free(words[i]);
+	}
+
 	free(words);
+	free(arr);
 
 	return 0;
 }
